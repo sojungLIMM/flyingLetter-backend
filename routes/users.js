9f00e99cf51c8256302bf3308c3df6e1571153db
sojungLIMM <thwid9897@gmail.com>
2022-03-02 20:42:35 +0900

    feat: make get users letters

--- routes/users.js
+++ routes/users.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const { upload } = require("../middlewares/multer");
 const { verifyToken } = require("../middlewares/verifyToken");
-const { getAll, createLetter } = require("../controllers/users.controller");
+const { getAll, getLetters, createLetter } = require("../controllers/users.controller");
 
 router.get("/", verifyToken, getAll);
-router.post("/:userId/letters", verifyToken, upload.single("letterPaperImage"), createLetter);
+router.get("/:userId/letters/to", verifyToken, getLetters);
+router.post("/:userId/letters", verifyToken, upload.single("letterWallPaper"), createLetter);
 
 module.exports = router;
